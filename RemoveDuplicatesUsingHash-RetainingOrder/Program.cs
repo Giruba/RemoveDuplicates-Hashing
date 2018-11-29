161e07f78cd191b4baeb63167e8c76491fd79de1
gibeulah <gibeulah@microsoft.com>
2018-11-29 16:37:13 +0530

    First Commit

--- RemoveDuplicatesUsingHash-RetainingOrder/Program.cs
+++ RemoveDuplicatesUsingHash-RetainingOrder/Program.cs
@@ -6,7 +6,41 @@ namespace RemoveDuplicatesUsingHash_RetainingOrder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Remove duplicates in a string using Hashing!");
+            Console.WriteLine("--------------------------------------------");
+
+            Console.WriteLine("Enter the input string");
+            String inputString = Console.ReadLine();
+            Console.WriteLine();
+            String resultantString = GetDuplicatesRemovedString(inputString);
+            if (!string.IsNullOrEmpty(resultantString)) {
+                Console.WriteLine("The resultant string is "+resultantString);
+            }else{
+                Console.WriteLine("The input string is empty!");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static string GetDuplicatesRemovedString(string inputString) {
+            if (string.IsNullOrEmpty(inputString)) {
+                return inputString;
+            }
+            if (string.IsNullOrEmpty(inputString.Trim())) {
+                return inputString.Trim();
+            }
+            bool[] hashChar = new bool[256];
+            char[] charArray = new char[inputString.Length + 1];
+            int newIndex = 0;
+
+            for (int i = 0; i < inputString.Length; i++) {
+                if (hashChar[inputString[i]] == false) {
+                    hashChar[inputString[i]] = true;
+                    charArray[newIndex++] = inputString[i];
+                }
+            }
+            
+            return new string(charArray);
         }
     }
 }
